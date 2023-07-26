dcec3b7e6635c238b3a25e1a9d547a5f93f6a0a9
JonatasCopernico <jonatas.pimenta@copernico.com.br>
2023-07-26 15:10:22 -0300

    fix navbar zindex

--- src/ui/components/organisms/NavBar.tsx
+++ src/ui/components/organisms/NavBar.tsx
@@ -1,6 +1,6 @@
 export default function NavBar() {
   return (
-    <nav className="bg-gray-700 w-screen font-semibold fixed top-0 p-3 text-white">
+    <nav className="bg-gray-700 w-screen font-semibold fixed top-0 p-3 text-white z-20">
       <p>CUSTOMER EXPERIENCE MAPPING</p>
     </nav>
   );
