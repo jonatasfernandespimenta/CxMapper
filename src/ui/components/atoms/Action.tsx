3c5e88ae2ade31fd034716d3426f984a5fc4444d
JonatasCopernico <jonatas.pimenta@copernico.com.br>
2023-07-26 15:10:09 -0300

    makes actions collapsable

--- src/ui/components/atoms/Action.tsx
+++ src/ui/components/atoms/Action.tsx
@@ -1,19 +1,27 @@
+"use client";
+
+import { useState } from "react";
 import ActionHeader from "./ActionHeader";
 
 interface IAction {
   onClick?: () => void;
   ref?: any;
   icon: JSX.Element;
-  actionName: string
-  children?: JSX.Element
+  actionName: string;
+  children?: JSX.Element;
 }
 
 export default function Action({ icon, actionName, children }: IAction) {
-  return (
-    <div className="w-60 p-4 shadow rounded-md border-solid border-[1px] flex justify-center flex-col gap-4">
-      <ActionHeader icon={icon} actionName={actionName} />
+  const [isVisible, setIsVisible] = useState<boolean>(false);
 
-      {children}
+  return (
+    <div
+      onClick={() => setIsVisible(!isVisible)}
+      className="w-60 p-4 shadow rounded-md border-solid border-[1px] flex justify-center flex-col gap-4 cursor-pointer"
+    >
+      <ActionHeader icon={icon} isActive={isVisible} actionName={actionName} />
+
+      {isVisible && children}
     </div>
   );
 }
--- src/ui/components/atoms/ActionHeader.tsx
+++ src/ui/components/atoms/ActionHeader.tsx
@@ -1,22 +1,28 @@
-import Divider from "./Divider"
+import Divider from "./Divider";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 interface IActionHeader {
-  icon: JSX.Element
-  actionName: string
+  icon: JSX.Element;
+  actionName: string;
+  isActive: boolean;
 }
 
-export default function ActionHeader({ icon, actionName }: IActionHeader) {
-  return(
+export default function ActionHeader({
+  icon,
+  actionName,
+  isActive,
+}: IActionHeader) {
+  return (
     <div className="flex flex-col items-center gap-2">
       <div className="flex flex-row items-center gap-2">
-        <div className="bg-green-200 p-1 rounded">
-          {icon}
-        </div>
+        <div className="bg-green-200 p-1 rounded">{icon}</div>
 
         <p className="text-black font-semibold">{actionName}</p>
+
+        {isActive ? <FaChevronUp /> : <FaChevronDown />}
       </div>
 
       <Divider />
     </div>
-  )
+  );
 }
