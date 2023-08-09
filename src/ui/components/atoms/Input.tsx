e1fdc1117202e0797803fb4f1cb83588741aa411
Jonatas <Jonatas@Marcio-Lippa-Sol-Mais.local>
2023-08-09 08:49:56 -0300

    changes password input type to password

--- src/ui/components/atoms/Input.tsx
+++ src/ui/components/atoms/Input.tsx
@@ -1,16 +1,17 @@
-interface IInput {
+interface IInput extends React.InputHTMLAttributes<HTMLInputElement> {
   value: any;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
 }
 
-export default function Input({ onChange, value, placeholder }: IInput) {
+export default function Input({ onChange, value, placeholder, ...rest }: IInput) {
   return (
     <input
       value={value}
       onChange={onChange}
       className="w-full rounded-md p-1 border-solid border-[#DADCE0] border-[1px] focus:outline-none focus:ring focus:ring-green-300"
       placeholder={placeholder}
+      {...rest}
     />
   );
 }
--- src/ui/components/molecules/LoginForm.tsx
+++ src/ui/components/molecules/LoginForm.tsx
@@ -7,13 +7,13 @@ import Link from "next/link";
 export default function LoginForm() {
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
-  
-  return(
+
+  return (
     <div className="bg-green-500 p-8 shadow-md rounded-md gap-4 flex flex-col ">
       <img src="/logo.png" alt="logo" width={350} />
 
       <Input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="E-mail..." />
-      <Input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password..." />
+      <Input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password..." type="password" />
 
       <Link href={'/maps'} className="w-full">
         <button className="p-2 w-full bg-green-200 rounded">
